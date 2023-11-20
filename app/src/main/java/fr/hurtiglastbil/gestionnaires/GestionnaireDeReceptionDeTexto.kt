a40615c2df0e1b5075919d20e690395cdc1dbb2e
BoiteEnCarton <91901038+BoiteEnCarton@users.noreply.github.com>
2023-11-20 08:51:59 +0100

    Feature stockage externe (#13)
    * Enregistrement du SMS reçu dans le stockage externe (dans Documents)
    
    * Refactor du nom de la fonction enregistrerLeFichier
    
    * Changement du AndroidManifest.xml

--- app/src/main/java/fr/hurtiglastbil/gestionnaires/GestionnaireDeReceptionDeTexto.kt
+++ app/src/main/java/fr/hurtiglastbil/gestionnaires/GestionnaireDeReceptionDeTexto.kt
@@ -1,13 +1,18 @@
 package fr.hurtiglastbil.gestionnaires
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.os.Environment
+import android.provider.MediaStore
 import android.provider.Telephony
 import android.util.Log
 import fr.hurtiglastbil.enumerations.TagsErreur
 import fr.hurtiglastbil.modeles.Configuration
 import fr.hurtiglastbil.modeles.Personne
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 
@@ -24,7 +29,7 @@ fun traiterTexto(contexte: Context?, action: Intent?) {
                 Log.d("Récepteur de Texto", "Texto reçu de $expediteur  : $corpsDuMessage")
                 Log.d("WHITELIST", "traiterTexto: $expediteur est dans la liste blanche")
 
-                EnregistrerLeFichier(contexte, expediteur, horodatage, corpsDuMessage)
+                enregistrerLeFichier(contexte, expediteur, horodatage, corpsDuMessage)
             } else {
                 Log.d("WHITELIST", "traiterTexto: $expediteur n'est pas dans la liste blanche")
             }
@@ -35,13 +40,13 @@ fun traiterTexto(contexte: Context?, action: Intent?) {
     }
 }
 
-private fun EnregistrerLeFichier(
-    contexte: Context?,
+private fun enregistrerLeFichier(
+    contexte: Context,
     expediteur: String?,
     horodatage: Long,
     corpsDuMessage: String?
 ) {
-    val fichier = File(contexte?.filesDir, "${expediteur}_${horodatage}_log.txt")
+    val fichier = File(contexte.getExternalFilesDir("hurtiglastbil"), "${expediteur}_${horodatage}_log.txt")
     if (!fichier.exists()) {
         try {
             fichier.createNewFile()
@@ -51,7 +56,7 @@ private fun EnregistrerLeFichier(
     }
     // Contenu à écrire dans le fichier
     val texteDansFichierDeLog = "SMS reçu de $expediteur : $corpsDuMessage\n"
-    Log.d("Récepteur de Texto", contexte?.filesDir.toString())
+    Log.d("Récepteur de Texto", contexte.filesDir.toString())
     Log.d("Récepteur de Texto", fichier.readText())
     try {
         val fluxDeFichierSortant = FileOutputStream(
@@ -63,5 +68,31 @@ private fun EnregistrerLeFichier(
         fluxDeFichierSortant.close()
     } catch (e: IOException) {
         Log.e(TagsErreur.ERREUR_MODIFICATION_FICHIER.tag, TagsErreur.ERREUR_MODIFICATION_FICHIER.message + " de stockage de SMS." )
+    }
+    updateGallery(contexte, fichier)
+}
+
+private fun updateGallery(context: Context, file: File) {
+    val resolver: ContentResolver = context.contentResolver
+    val contentValues = ContentValues()
+    contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, file.name)
+    contentValues.put(MediaStore.Images.Media.MIME_TYPE, "text/plain")
+    contentValues.put(
+        MediaStore.Images.Media.RELATIVE_PATH,
+        Environment.DIRECTORY_DOCUMENTS + "/hurtiglastbil"
+    )
+    val uri = resolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+    try {
+        resolver.openOutputStream(uri!!)?.use { outputStream ->
+            FileInputStream(file).use { fileInputStream ->
+                val buffer = ByteArray(1024)
+                var bytesRead: Int
+                while (fileInputStream.read(buffer).also { bytesRead = it } != -1) {
+                    outputStream.write(buffer, 0, bytesRead)
+                }
+            } // Le flux fileInputStream est fermé automatiquement à la fin du bloc 'use'
+        } // Le flux outputStream est fermé automatiquement à la fin du bloc 'use'
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
 }
