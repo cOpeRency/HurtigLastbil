ad21708c27aee8b3d1075e2eb994a35e14c84277
Paul Windows <paul.rezzonico@windows.com>
2024-01-26 10:11:17 +0100

    Refact(BuildGradle) hardcore version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,21 @@
+val androidCoreVersion="1.12.0"
+val androidAppCompatVersion="1.6.1"
+val androidMaterialVersion="1.11.0"
+val androidConstraintLayoutVersion="2.1.4"
+val junitVersion="4.13.2"
+val assertJVersion="3.24.2"
+val mockkVersion="1.13.8"
+val robolectricVersion="4.11.1"
+val espressoVersion="3.5.1"
+val kotlinSerializationVersion="1.6.1"
+val junit5Version="5.8.1"
+val espressoCoreVersion="3.5.1"
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.sonarqube") version "4.4.1.3373"
     kotlin("plugin.serialization") version "1.9.10"
 }
 
@@ -34,16 +49,25 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("org.robolectric:robolectric:4.11.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:$androidCoreVersion")
+    implementation("androidx.appcompat:appcompat:$androidAppCompatVersion")
+    implementation("com.google.android.material:material:$androidMaterialVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$androidConstraintLayoutVersion")
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    androidTestImplementation("androidx.test.ext:junit:$junit5Version")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoCoreVersion")
+    testImplementation("org.robolectric:robolectric:$robolectricVersion")
+}
 
+sonar {
+    properties {
+        property("sonar.projectName", "HurtigLastbil")
+        property("sonar.projectKey", "HurtigLastbil")
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.token", "sqp_dc869d0b3da5b1ef333c636b5550a11d7a71f941")
+        property("sonar.gradle.skipCompile", "true")
+    }
 }
