5f078e35a13b71df45c81e9dd10e9918aaff340a
Paul Windows <paul.rezzonico@windows.com>
2024-01-26 11:38:39 +0100

    Refact(SDK check) remove unused check on sdk

--- app/src/main/java/fr/hurtiglastbil/activites/ActivitePrincipale.kt
+++ app/src/main/java/fr/hurtiglastbil/activites/ActivitePrincipale.kt
@@ -2,11 +2,9 @@ package fr.hurtiglastbil.activites
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
-import android.view.WindowManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import fr.hurtiglastbil.R
@@ -25,15 +23,7 @@ class ActivitePrincipale : AppCompatActivity() {
         // Masquer la barre de statut
         window.decorView.alpha = 0f
 
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
 
         if (ContextCompat.checkSelfPermission(
                 this,
